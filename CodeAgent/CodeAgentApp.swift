bf7db0afbc3dbfb54bfaed656c6babda2a23559a
xiaoyuan <seyooe@gmail.com>
2026-06-24 17:33:33 +0800

    Initial Commit

---
+++ CodeAgent/CodeAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeAgentApp.swift
+//  CodeAgent
+//
+//  Created by xiaoyuan on 2026/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
